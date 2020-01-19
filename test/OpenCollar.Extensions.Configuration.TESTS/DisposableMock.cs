ed6a3b29f9489891926bb70e9ed864fcbd6b3925
Jonathan Evans <jevans@open-collar.org.uk>
2020-01-19 12:02:56 +0000

    Added unit tests for Disposable.

---
+++ test/OpenCollar.Extensions.Configuration.TESTS/DisposableMock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenCollar.Extensions.Configuration.TESTS
+{
+    internal class DisposableMock : Disposable
+    {
+        private int _disposeCalls;
+
+        public int DisposeCalls
+        {
+            get
+            {
+                return _disposeCalls;
+            }
+        }
+
+        public new bool IsDisposed
+        {
+            get
+            {
+                return base.IsDisposed;
+            }
+        }
+
+        public new void EnforceDisposed()
+        {
+            base.EnforceDisposed();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            ++_disposeCalls;
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ test/OpenCollar.Extensions.Configuration.TESTS/DisposableTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace OpenCollar.Extensions.Configuration.TESTS
+{
+    public class DisposableTests
+    {
+        [Fact]
+        public void TestDispose()
+        {
+            var x = new DisposableMock();
+
+            Assert.NotNull(x);
+            x.Dispose();
+            Assert.Equal(1, x.DisposeCalls);
+            x.Dispose();
+            Assert.Equal(1, x.DisposeCalls);
+        }
+
+        [Fact]
+        public void TestEnforceDisposed()
+        {
+            var x = new DisposableMock();
+
+            Assert.NotNull(x);
+            x.EnforceDisposed();
+            x.Dispose();
+            Assert.Throws<ObjectDisposedException>(() => x.EnforceDisposed());
+        }
+
+        [Fact]
+        public void TestIsDisposed()
+        {
+            var x = new DisposableMock();
+
+            Assert.NotNull(x);
+            Assert.False(x.IsDisposed);
+            x.Dispose();
+            Assert.True(x.IsDisposed);
+        }
+    }
+}
