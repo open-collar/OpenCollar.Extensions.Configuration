3ada36d4844a18831708f8888a6e902268d75460
Jonathan Evans <jevans@open-collar.org.uk>
2020-02-06 23:08:37 +0000

    Added tests for the PathHelper class.

---
+++ test/OpenCollar.Extensions.Configuration.TESTS/PathHelperTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace OpenCollar.Extensions.Configuration.TESTS
+{
+    public sealed class PathHelperTests
+    {
+        [Fact]
+        public void TestGetPath()
+        {
+            Assert.Equal(string.Empty, PathHelper.GetPath());
+            Assert.Equal(string.Empty, PathHelper.GetPath(string.Empty));
+            Assert.Equal(string.Empty, PathHelper.GetPath(null));
+            Assert.Equal(string.Empty, PathHelper.GetPath(new string[0]));
+            Assert.Equal("A:B", PathHelper.GetPath("A", "B"));
+            Assert.Equal("A:B", PathHelper.GetPath("A", string.Empty, "B"));
+            Assert.Equal("A:B", PathHelper.GetPath("A", null, "B"));
+        }
+    }
+}
