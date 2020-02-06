10b4ccd1ef81939255bae0c726f51b9e8b202939
Jonathan Evans <jevans@open-collar.org.uk>
2020-02-06 22:57:15 +0000

    Added unit tests for PathAttribute.

---
+++ test/OpenCollar.Extensions.Configuration.TESTS/PathAttributeTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace OpenCollar.Extensions.Configuration.TESTS
+{
+    public sealed class PathAttributeTests
+    {
+        [Fact]
+        public void TestConstructor()
+        {
+            const string path = @"PATH";
+            var x = new PathAttribute(PathIs.Root, path);
+
+            Assert.NotNull(x);
+            Assert.Equal(path, x.Path);
+            Assert.Equal(PathIs.Root, x.Usage);
+
+            Assert.Throws<ArgumentException>(() => { new PathAttribute(PathIs.Root, null); });
+            Assert.Throws<ArgumentException>(() => { new PathAttribute(PathIs.Root, string.Empty); });
+            Assert.Throws<ArgumentOutOfRangeException>(() => { new PathAttribute(PathIs.Unknown, path); });
+            Assert.Throws<ArgumentOutOfRangeException>(() => { new PathAttribute((PathIs)666, path); });
+        }
+    }
+}
