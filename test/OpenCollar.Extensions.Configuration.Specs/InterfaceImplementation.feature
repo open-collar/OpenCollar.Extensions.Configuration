8b8ad2381703fb349df24e07188b834e7475be94
Jonathan Evans <jevans@open-collar.org.uk>
2020-02-11 22:45:27 +0000

    Started implementing SpecFlow tests.

---
+++ test/OpenCollar.Extensions.Configuration.Specs/InterfaceImplementation.feature
@@ -0,0 +1,11 @@
+﻿Feature: Interface Implementation
+    In order to access configuration values
+    As a configuration consumer
+    I want to access configuration values through a strongly-typed interface
+
+@interface
+Scenario: Read an Int32 property value
+	Given a property name
+	And a sample value
+	When the value of the property is read
+	Then the result must equal the sample value
---
+++ test/OpenCollar.Extensions.Configuration.Specs/InterfaceImplementationSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace OpenCollar.Extensions.Configuration.Specs
+{
+    [Binding]
+    public class InterfaceImplementationSteps
+    {
+        [Given(@"a property name")]
+        public void GivenAPropertyName()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"a sample value")]
+        public void GivenASampleValue()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the value of the property is read")]
+        public void WhenTheValueOfThePropertyIsRead()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result must equal the sample value")]
+        public void ThenTheResultMustEqualTheSampleValue()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
