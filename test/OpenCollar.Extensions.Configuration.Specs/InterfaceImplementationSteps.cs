dcfc1b2208e7ff3e9572a68b0638dc645df3ca38
Jonathan Evans <jevans@open-collar.org.uk>
2020-02-12 08:03:56 +0000

    Started to look at SpecFlow testing.

--- test/OpenCollar.Extensions.Configuration.Specs/InterfaceImplementationSteps.cs
+++ test/OpenCollar.Extensions.Configuration.Specs/InterfaceImplementationSteps.cs
@@ -9,25 +9,25 @@ namespace OpenCollar.Extensions.Configuration.Specs
         [Given(@"a property name")]
         public void GivenAPropertyName()
         {
-            ScenarioContext.Current.Pending();
+            //ScenarioContext.Current.Pending();
         }
-        
+
         [Given(@"a sample value")]
         public void GivenASampleValue()
         {
-            ScenarioContext.Current.Pending();
+            //ScenarioContext.Current.Pending();
         }
-        
-        [When(@"the value of the property is read")]
-        public void WhenTheValueOfThePropertyIsRead()
-        {
-            ScenarioContext.Current.Pending();
-        }
-        
+
         [Then(@"the result must equal the sample value")]
         public void ThenTheResultMustEqualTheSampleValue()
         {
-            ScenarioContext.Current.Pending();
+            //ScenarioContext.Current.Pending();
+        }
+
+        [When(@"the value of the property is read")]
+        public void WhenTheValueOfThePropertyIsRead()
+        {
+            //ScenarioContext.Current.Pending();
         }
     }
 }
